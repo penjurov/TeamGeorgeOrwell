9d24d5287cea2fac59c6a61bdaf09f54419c80f2
Sh0nZi <sss17@mail.com>
2014-02-14 00:16:50 +0200

    Update

---
+++ RPG/RPG/RPG/Interfaces/IUpdateable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Rpg.Interfaces
+{
+    public interface IUpdatable
+    {
+        float Speed { get; }
+
+        float Rotation { get; set; }
+
+        bool Alive { get; set; }
+    }
+}
