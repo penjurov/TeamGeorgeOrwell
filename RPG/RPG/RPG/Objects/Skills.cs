0bca0dc28b686e0da710e6a37a02c086e3b65516
Cvetan123 <c_s_g@mail.bg>
2014-02-21 10:15:15 +0200

    Skill class updated

--- RPG/RPG/RPG/Objects/Skills.cs
+++ RPG/RPG/RPG/Objects/Skills.cs
@@ -4,9 +4,48 @@
 
     public class Skills : Obj
     {
+        private int skillLevel;
+        private int skillCost;
+
         public Skills(Vector2 pos) : base(pos)
         {
             this.Position = pos;
+            this.SkillLevel = 1;
+            this.skillCost = 50;
+        }
+
+        public void UseSkill()
+        {
+
+        }
+
+        public void LevelUp()
+        {
+            this.SkillLevel++;
+        }
+
+        public int SkillLevel 
+        {
+            get
+            {
+                return this.skillLevel;
+            }
+            set
+            {
+                this.skillLevel = value;
+            }
+        }
+
+        public int SkillCost 
+        {
+            get
+            {
+                return this.skillCost;
+            }
+            set
+            {
+                this.skillCost = value;
+            }
         }
     }
 }
