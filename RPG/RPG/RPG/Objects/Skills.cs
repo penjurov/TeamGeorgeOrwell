12e3a43f09ee711f22e0f800686d7aa0012758fe
Cvetan123 <c_s_g@mail.bg>
2014-02-21 10:40:37 +0200

    Skill types added

--- RPG/RPG/RPG/Objects/Skills.cs
+++ RPG/RPG/RPG/Objects/Skills.cs
@@ -2,21 +2,36 @@
 {
     using Microsoft.Xna.Framework;
 
+    public enum SkillType { Heal, CollateralDamage, Hit };
+
     public class Skills : Obj
     {
         private int skillLevel;
         private int skillCost;
+        private readonly SkillType skillType;
 
-        public Skills(Vector2 pos) : base(pos)
+        public Skills(Vector2 pos, SkillType skillType)
+            : base(pos)
         {
             this.Position = pos;
             this.SkillLevel = 1;
-            this.skillCost = 50;
+            this.SkillCost = 50;
+            this.skillType = skillType;
         }
 
         public void UseSkill()
         {
-
+            switch (this.skillType)
+            {
+                case SkillType.Heal:
+                    break;
+                case SkillType.CollateralDamage:
+                    break;
+                case SkillType.Hit:
+                    break;
+                default:
+                    break;
+            }
         }
 
         public void LevelUp()
